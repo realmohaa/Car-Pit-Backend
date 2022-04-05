e81ae50381e6493181da8e9011796147f22ea447
Mohammed Abufarhana <realmohaa@mailbox.unideb.hu>
2022-04-05 03:17:24 +0200

    Added monthly /stats with Mongo Aggregation

--- api/routes/user.js
+++ api/routes/user.js
@@ -29,7 +29,7 @@ router.get("/", verifyAdmin, async (req,res) => {
 })
 
 // User
-router.get("/:id", verifyAuth, async(req,res) => {
+router.get("/find/:id", verifyAuth, async(req,res) => {
     try {
         await User.findById(req.params.id)
             .exec()
@@ -75,5 +75,31 @@ router.delete("/:id", verifyAuth, async (req,res) => {
         res.status(500).json(err);
     }
 })
+
+// Users Stats
+router.get("/stats", verifyAdmin, async (req, res) => {
+    const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+  
+    try {
+      const data = await User.aggregate([
+        { $match: { createdAt: { $gte: lastYear } } },
+        {
+          $project: {
+            month: { $month: "$createdAt" },
+          },
+        },
+        {
+          $group: {
+            _id: "$month",
+            total: { $sum: 1 },
+          },
+        },
+      ]);
+      res.status(200).json(data)
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router
